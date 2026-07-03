fd1e39201d9b681f6b028b958730dee3631c3b74
Udita Chakraborty <udita.chakraborty05@gmail.com>
2026-07-03 10:59:52 +0530

    Exercise 1 - Implementing the Singleton Pattern

---
+++ deep-skilling/engineering-concepts/design-principles-patterns/singleton-pattern/SingletonTest.java
@@ -0,0 +1,20 @@
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        // Get Logger instance
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application started.");
+
+        // Get Logger instance again
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Application running.");
+
+        // Check if both references point to the same object
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+    }
+}
