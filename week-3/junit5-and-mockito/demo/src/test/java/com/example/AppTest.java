9ecbcac5e7855ff63918ce9ed7e01ca078d9b360
Udita Chakraborty <udita.chakraborty05@gmail.com>
2026-07-10 12:43:55 +0530

    JUnit5 Setup

---
+++ week-3/junit5-and-mockito/demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
